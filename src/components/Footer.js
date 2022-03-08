1dc60b29bf2f605c991e502e0f0aec056c2e2954
typsz <grzeg2010@interia.eu>
2022-03-08 21:55:45 +0100

    Biały styl dla stopki

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import { Link, Route } from "react-router-dom";
 
 const Footer = () => {
     return(
-        <footer className="ui inverted footer segment">
+        <footer className="ui footer segment">
             <div className="ui container grid centered">
-                <a href="https://github.com/typsz/react-sklep" className="inverted">
+                <a href="https://github.com/typsz/react-sklep">
                     <i className="big github icon"></i>
                 </a>
             </div>
